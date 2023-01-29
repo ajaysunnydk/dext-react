fb0817ef06fd73188af16593bc64d9951c2a636d
ajaysunnydk <ajaysunnydk167@gmail.com>
2023-01-30 00:19:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React, { useState } from 'react'
 import Select from 'react-select'
 import Header from './Header';
-require('axios');
 import js_beautify from 'js-beautify'
 
 function App() {
@@ -19,6 +18,8 @@ function App() {
   var reqBody;
   var data;
   const [respData, setRespData] = useState('');
+  
+  const axios = require('axios');
 
   const handleSubmit = (event) => {
     event.preventDefault();
